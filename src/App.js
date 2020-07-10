cd1a29bfe4d16ef60aaffdd742212bb26c7fee31
bilalhadid <hadid0648@gmail.com>
2020-07-10 19:16:10 +0500

    Router

--- src/App.js
+++ src/App.js
@@ -2,11 +2,24 @@ import React from "react";
 import "./App.css";
 import Navbar from "./component/navbar";
 import { Cards } from "./component/card";
+import { Routes, Route } from "react-router";
+import { Link } from "react-router-dom";
+
+function NotFound() {
+  return <div>Not Found</div>;
+}
+
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Cards />
+      <div>
+        <Link to="/">Navbar</Link> <Link to="card">Card</Link>
+      </div>
+      <Routes>
+        <Route path="/" element={<Navbar />} />
+        <Route path="card" element={<Cards />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 }
