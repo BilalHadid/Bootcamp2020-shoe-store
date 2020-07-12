a02c3cb84f9d8516245b9ca8ff7f9d9d999dac72
bilalhadid <hadid0648@gmail.com>
2020-07-12 20:31:49 +0500

    n

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,10 +1,54 @@
 import React from "react";
 import "../App.css";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Typography from "@material-ui/core/Typography";
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+    border: "2px",
+  },
+  media: {
+    height: 190,
+  },
+  line: {
+    borderTop: "1px solid white",
+    width: "10%",
+    marginLeft: "15vh",
+  },
+});
 
 export const Home = () => {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <div className="Home1">Home</div>
+    <div>
+      <div className="Home1">
+        <div className="Main">
+          <h1>Casual Sport Shoes</h1>
+          <h1 className={classes.line}></h1>
+        </div>
+      </div>
+      <Card className={classes.root}>
+        <CardActionArea>
+          <CardContent style={{ textAlign: "center" }}>
+            <Typography gutterBottom variant="h5" component="h2">
+              Bought From
+            </Typography>
+            <Typography variant="h5" style={{ color: "red" }}>
+              Islamabad
+            </Typography>
+          </CardContent>
+          <CardMedia
+            className={classes.media}
+            image="https://cdn.shopify.com/s/files/1/0143/1552/0054/products/681-2162-c_2400x.jpg?v=1569390463"
+            title="Contemplative Reptile"
+          />
+        </CardActionArea>
+      </Card>
     </div>
   );
 };
