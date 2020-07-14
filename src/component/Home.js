7a11dd441d1f950c3f600a1cabb8dd3c2f6dc366
bilalhadid <hadid0648@gmail.com>
2020-07-15 00:31:57 +0500

    Complete home

--- src/component/Home.js
+++ src/component/Home.js
@@ -14,7 +14,13 @@ import CardActionArea from "@material-ui/core/CardActionArea";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import Typography from "@material-ui/core/Typography";
-
+import FacebookIcon from "@material-ui/icons/Facebook";
+import InstagramIcon from "@material-ui/icons/Instagram";
+import { IconButton } from "@material-ui/core";
+import TwitterIcon from "@material-ui/icons/Twitter";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import PinterestIcon from "@material-ui/icons/Pinterest";
+import YouTubeIcon from "@material-ui/icons/YouTube";
 const useStyles = makeStyles((theme) => ({
   root: {
     maxWidth: 345,
@@ -35,6 +41,14 @@ const useStyles = makeStyles((theme) => ({
     width: "100%",
     height: "30vh",
   },
+  foot: {
+    backgroundColor: "#565C68",
+  },
+  bottom: {
+    backgroundColor: "#161B27",
+    color: "white",
+    padding: "2vh",
+  },
 }));
 
 const zoomOutProperties = {
@@ -48,7 +62,6 @@ const zoomOutProperties = {
 const images = [img1, img2, img3, img4, img5, img6];
 export const Home = () => {
   const classes = useStyles();
-
   return (
     <div>
       <div className="slide-container">
@@ -110,31 +123,61 @@ export const Home = () => {
         </Card>
       </div>
       <br />
-      <div>
-        <h2>LATEST COLLECTION</h2>
-        <hr></hr>
+      <footer className={classes.foot}>
+        <IconButton color="inherit" aria-label="add to shopping cart">
+          <FacebookIcon />
+        </IconButton>
+        <IconButton color="inherit" aria-label="add an alarm">
+          <InstagramIcon />
+        </IconButton>
+        <IconButton color="inherit" aria-label="add to shopping cart">
+          <TwitterIcon />
+        </IconButton>
+        <IconButton color="inherit" aria-label="add to shopping cart">
+          <LinkedInIcon />
+        </IconButton>
+        <IconButton color="inherit" aria-label="add an alarm">
+          <PinterestIcon />
+        </IconButton>
+        <IconButton color="inherit" aria-label="add to shopping cart">
+          <YouTubeIcon />
+        </IconButton>
+        <div className={classes.bottom}>
+          <Typography>
+            Made by Bilal Hadid Link: bootcamp2020-shoe-store.surge.sh
+          </Typography>
+        </div>
 
-        <div className="div1" id="divi">
-          linear
-        </div>
-        <br></br>
-        <div className="div2" id="divi">
-          ease
-        </div>
-        <br></br>
-        <div className="div3" id="divi">
-          ease-in
-        </div>
-        <br></br>
-        <div className="div4" id="divi">
-          ease-out
-        </div>
-        <br></br>
-        <div className="div5" id="divi">
-          ease-in-out
-        </div>
-        <br></br>
-      </div>
+        {/* <Typography variant="h6" marked="left" gutterBottom>
+          Legal
+        </Typography>
+        <ul className={classes.list}>
+          <li className={classes.listItem}>
+            <Link href="/premium-themes/onepirate/terms/">Terms</Link>
+          </li>
+          <li className={classes.listItem}>
+            <Link href="/premium-themes/onepirate/privacy/">Privacy</Link>
+          </li>
+        </ul>
+        <Typography variant="h6" marked="left" gutterBottom>
+          Language
+        </Typography>
+        <TextField
+          id="outlined-select-currency"
+          select
+          label="Select"
+          value={currency}
+          onChange={handleChange}
+          helperText="Please select your Language"
+          variant="outlined"
+        >
+          {currencies.map((option) => (
+            <MenuItem key={option.value} value={option.value}>
+              {option.label}
+            </MenuItem>
+          ))}
+        </TextField> */}
+      </footer>
     </div>
   );
 };
